20372d68099b17cfb4ea239f8352be4d3439b59a
Paul Hubbard <paul.hubbard@verizon.com>
2012-07-26 15:45:10 -0700

    save state

--- src/net/phfactor/meltdown/GroupsActivity.java
+++ src/net/phfactor/meltdown/GroupsActivity.java
@@ -1,19 +1,56 @@
 package net.phfactor.meltdown;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.Toast;
 
-public class GroupsActivity extends Activity {
-
+public class GroupsActivity extends Activity 
+{
+	private SharedPreferences prefs;
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
+        
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // Check for login, run prefs        
+        if (haveSetup() == true)
+        {
+        	startActivity(new Intent(this, PreferencesActivity.class));
+        	Toast.makeText(this, "Please configure a server", Toast.LENGTH_SHORT).show();
+        }
+        
+        // try login
         setContentView(R.layout.activity_groups);
+        
+        RestClient rc = new RestClient(this);
+        if (rc.tryLogin())
+        	Toast.makeText(this, "Logged in OK!", Toast.LENGTH_LONG).show();
+        else
+        {
+        	Toast.makeText(this, "Error on login. Please check preferences.", Toast.LENGTH_LONG).show();
+        	startActivity(new Intent(this, PreferencesActivity.class));
+        }
     }
 
+    private Boolean haveSetup()
+    {
+    	if (prefs.getString("prefUsername", null) == null)
+    		return false;
+    	
+    	if (prefs.getString("prefServerUrl", null) == null)
+    		return false;
+    	
+    	if (prefs.getString("prefPassword", null) == null)
+    		return false;
+    	
+    	return true;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_groups, menu);
