8d88856b3b8444412f3d08f56fe0f5beceb9aef2
Paul Hubbard <paul.hubbard@verizon.com>
2012-10-17 18:13:53 -0700

    sync to laptop

--- src/net/phfactor/meltdown/RssFeed.java
+++ src/net/phfactor/meltdown/RssFeed.java
@@ -1,5 +1,8 @@
 package net.phfactor.meltdown;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,6 +23,9 @@ public class RssFeed
 	public  Boolean is_spark;
 	public  long last_updated_on_time;
 	
+	// Derived reverse index
+	public List<Integer> groups;
+	
 	public RssFeed(JSONObject feed)
 	{
 		try
@@ -33,6 +39,7 @@ public class RssFeed
 			this.favicon_id = feed.getInt("favicon_id");
 			this.is_spark = (feed.getInt("is_spark") == 0);
 			this.last_updated_on_time = feed.getLong("last_updated_on_time");			
+			this.groups = new ArrayList<Integer>();
 			
 		}catch (JSONException je)
 		{
